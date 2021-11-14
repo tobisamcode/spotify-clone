5ebbb22e40f8f361316a26f1509eeb40dfb84462
tobisamcode <samueloluwatobiloba48@gmail.com>
2021-11-14 16:36:15 +0100

    second

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="bg-dark h-screen">
+    <h1 class="text-red ">Hello</h1>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+
+    }
+  },
+ 
+
+
 }
 </script>
-
-<style src="./assets/tailwind.css">
-
-</style>
