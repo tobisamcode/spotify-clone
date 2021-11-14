ed6f1085e5796191fe63dda44a0a0dc40fa557cb
tobisamcode <samueloluwatobiloba48@gmail.com>
2021-11-15 00:31:15 +0100

    changes

--- src/App.vue
+++ src/App.vue
@@ -18,20 +18,31 @@
             <h1 class="mb-3 text-xs text-lightest tracking-widest uppercase">playlists</h1>
             <button class="flex items-center justify-start opacity-75 hover:opacity-100 mb-2">
               <img src="addBox.png" class="h-8 w-8 mr-3"/>
-              <p class="text-sm text-white font-semibold">Create Playlist</p>
+              <p class="text-xs text-white font-semibold">Create Playlist</p>
             </button>
             <button class="flex items-center justify-start opacity-75 hover:opacity-100">
               <img src="like.png" class="h-8 w-8 mr-3" style="filter: brightness(0) invert(1);"/>
-              <p class="text-sm text-white font-semibold">Liked Songs</p>
+              <p class="text-xs text-white font-semibold">Liked Songs</p>
             </button>
             <div class="h-px w-full bg-light my-3"></div>
         </div>
 
         <div class="mx-5">
           <div class="w-full h-10 overflow-y-scroll ">
-            <p v-for="album in albums" :key="album.name" class="text-lightest hover:text-white text-xs py-1"> {{ album.name}} </p>
-
+            <p v-for="album in albums" :key="album.name" class="text-lightest hover:text-white text-sm py-1"> {{ album.name}} </p>
           </div>
+          <button class="flex items-center justify-start text-lightest hover:text-white py-2">
+            <i class="material-icons mr-3 rounded-full border border-lightest text-xs">arrow_downward</i>
+            <p class="text-sm font-semibold">Install App</p>
+          </button>
+        </div>
+        <div class="relative pt-4 ">
+          <div class="w-full h-full flex justify-end items-start opacity-0 hover:opacity-100 absolute p-2">
+            <div class="bg-black rounded-full h-6 w-6">
+              <i class="material-icons text-white">keyboard_arrow_down</i>
+            </div>
+          </div>
+          <img src="singer1.png" class=" w-full" style="height: 90px" />
         </div>
 
       </div>
@@ -39,8 +50,24 @@
       <!-- main content -->
       <div class="w-full h-full relative">
         <!-- header -->
-        <div class="w-full sticky top-0 p-2">
+        <div class="w-full sticky top-0 px-6 py-4 flex items-center justify-between ">
+            <div class="flex items-center">
+              <button class="rounded-full bg-black w-8 h-8 text-white mr-3 "><i class="material-icons text-3xl">keyboard_arrow_left</i></button>
+              <button class="rounded-full bg-black w-8 h-8 text-white "><i class="material-icons text-3xl">keyboard_arrow_right</i></button>
+            </div>
+            <div class="relative">
+              <button @click="showDropdown = true" class="bg-light rounded-full py-1 px-2 flex items-center ">
+                <img src="singer2.png" class="h-6 w-6 mr-2 rounded-full" />
+                <p class="text-white font-semibold text-xs mr-3 ">Tobi Samuel</p>
+                <i v-if="showDropdown === false" class="material-icons text-white">arrow_drop_down</i>
+                <i v-if="showDropdown === true" class="material-icons text-white">arrow_drop_up</i>
 
+              </button>
+              <div v-if="showDropdown === true" class="absolute bg-light w-full rounded mt-1">
+                <button  @click="showDropdown = false" class="w-full text-sm py-2 text-lightest hover:text-white border-b border-white opacity-50 hover:opacity-100 ">Account</button>
+                <button @click="showDropdown = false" class="w-full text-sm py-2 text-lightest hover:text-white ">Log out</button>
+              </div>
+            </div>
         </div>
       </div>
 
@@ -74,7 +101,8 @@ export default {
         {name: 'Mellow'},
         {name: 'Classic Road Trip SOngs'},
         {name: 'Lana Del Rey Radio'}
-      ]
+      ],
+      showDropdown: false
     }
   },
  
